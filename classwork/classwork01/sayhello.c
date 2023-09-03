7db631e62121506d9d3620b009fbb5f9fae68004
JeronAlford44 <jeron.alford@gmail.com>
2023-09-02 21:55:48 -0700

    gcc compiler error when using 'gets' function

--- classwork/classwork01/sayhello.c
+++ classwork/classwork01/sayhello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
-
-    printf("Hello World!");
+    char *name;
+    char input[25];
+    printf("Please Enter your name: ");
+             
+    if (gets(input)){
+        name = input;
+        printf("Hello %s!\n", name);
+        
+    } ;              
+    return 0;
 }
