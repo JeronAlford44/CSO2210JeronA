3d286ba8f1ddd4d312034c76d1404abd68bf8838
JeronAlford44 <jeron.alford@gmail.com>
2023-09-05 14:48:33 -0700

    using fgets instead of gets

--- classwork/classwork01/sayhello.c
+++ classwork/classwork01/sayhello.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[])
 {
     char *name;
     char input[25];
     printf("Please Enter your name: ");
-             
-    if (gets(input)){
-        name = input;
-        printf("Hello %s!\n", name);
-        
-    } ;              
+
+    fgets(input, 25, stdin);
+    
+    
+    printf("Hello %s!\n", input);
+    
     return 0;
 }
