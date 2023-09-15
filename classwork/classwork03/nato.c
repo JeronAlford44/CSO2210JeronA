057947d4fa9e459c5a502867cc92dfd61db6a64d
JeronAlford44 <jeron.alford@gmail.com>
2023-09-14 21:23:22 -0700

    trying nato.c

---
+++ classwork/classwork03/nato.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    char * letters[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char * nato[] = {'alpha','bravo','charlie','delta','echo','foxtrot','golf','hotel','india','juliett','kilo','lima','mike','november','oscar','papa','quebec','romeo','sierra','tango','uniform','victor','whiskey','xray','yankee','zulu'};
+    int i = 0;
+
+    char getIndex(char *letter){
+        for (int i =0; i<= 
+    }
+        //make a look to get the index of the letter that the user typed 
+       
+        //get the corresponding nato value with nato[letter]
+        //return the nato value and go to the next letter
+        //Do this process again for next word
+         
+        
+        
+        
+        
+      
+    
+    
+    // for (int i = 1; i <= argc; i++){
+    //     for (int j = 1; j < strlen(argv); j++){
+
+    //     }
+    //     printf("/n")
+    // }
+
+
+}
