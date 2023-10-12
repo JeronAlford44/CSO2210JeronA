33d6254033cfb4acfdb577db87f261576f6d0f34
jiamese <80025619+jiamese@users.noreply.github.com>
2023-10-12 07:12:58 -0700

    Update homework4.c
    2 and 3 added

--- homework/homework04/homework4.c
+++ homework/homework04/homework4.c
@@ -9,3 +9,28 @@ A  B  C  (A OR ((NOT A) AND B) AND C)   ((A NAND C) NAND A) = Y
 1  0  1                1                          1
 1  1  0                1                          0
 1  1  1                1                          1
+
+
+2)
+
+a1 ----|
+       |-AND--|
+b1 ----|      |
+       |      |
+       |      OR ---->
+       |      |
+a0 ----|      |
+       |-AND--|
+b0 ----|      |
+       |      |
+a0 ----|      |
+       |-AND--|
+b0 ----|      |
+3)
+a) value &= 0x55555555;
+b)value |= 0x00000007;
+c)remainder = value & 0x00000007;
+d)value = 0xFFFFFFFF;
+e)value ^= 0xC0000000;
+f)value &= 0xFFFFFFF8;
+
