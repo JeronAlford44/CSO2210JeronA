caa9be1e3af3f96626d3be90c3d8e2e4296f7ad2
Chris Beaudoin <109051003+Chris-Beaudoin@users.noreply.github.com>
2023-10-12 22:13:53 -0700

    Update homework4.c

--- homework/homework04/homework4.c
+++ homework/homework04/homework4.c
@@ -27,10 +27,36 @@ a0 ----|      |
        |-AND--|
 b0 ----|      |
 3)
-a) value &= 0x55555555;
+a) value &= 0xAAAAAAAA;
 b)value |= 0x00000007;
 c)remainder = value & 0x00000007;
 d)value = 0xFFFFFFFF;
 e)value ^= 0xC0000000;
 f)value &= 0xFFFFFFF8;
+
+4)
+
+
+5)
+
+
+
+7)
+
+
+
+8)
+JGZ 0x837BBE1     ; Jump if A > 0 to address 0x837BBE1
+JZ 0x837BBE1      ; Jump if A = 0 to address 0x837BBE1
+
+
+
+
+9) 
+1: The sequence swaps r8 and r9 w/ eachother, for example, the data that was in r8 will be in r9 and vice versa.
+
+2: When two numbers are XORed, and then you XOR the result, you will get the origional number back... This is in a sense, is taking the properties of the XOR to swap values without immediate storage.
+
+
+
 
